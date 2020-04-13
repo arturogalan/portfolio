0e453bcc9691596b65ca8d6b14edfd4779c9eddd
Arturo Galan <arturo.galan@gmail.com>
2020-04-13 16:46:13 +0200

    first blog iteration

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  
+        <div class="col">
+        <h3>This is the main layout</h3>
+          <router-view />
+        </div>
+</template>
+
+<script>
+// import SideNav from 'components/SideNav'
+// import Tail from 'components/Tail'
+
+export default {
+  name: 'MainLayout',
+  created () {
+    console.log('main layout');
+  },
+  // components: { SideNav, Tail },
+  data () {
+    return {
+      drawerOpened: false
+    }
+  },
+  computed: {
+    color () {
+      const { id, tag } = this.$route.params
+      return this.$colors[id || tag] || 'cyan'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import BlogEntries from '../statics/data/blogs.json'
+
+const blogRoutes = Object.keys(BlogEntries).map(section => {
+  const children = BlogEntries[section].map(child => ({
+    path: child.id,
+    name: child.id,
+    component: () => import(`../blogs/${section}/${child.id}.md`)
+  }))
+  return {
+    path: section,
+    name: section,
+    component: () => import('../layouts/BlogLayout.vue'),
+    children
+  }
+})
 
 Vue.use(VueRouter);
 
@@ -9,6 +24,24 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: '/blog',
+    component: () => import('../layouts/MainLayout.vue'),
+    children: blogRoutes,
+    // [
+      // {
+      //   path: '',
+      //   name: 'BlogRoot',
+      //   component: () => import('pages/Blogs.vue')
+      // },
+      // {
+      //   path: ':id',
+      //   name: 'BlogIndex',
+      //   component: () => import('pages/Blogs.vue')
+      // },
+      // ...blogRoutes
+    // ]
   },
   {
     path: "/about",
